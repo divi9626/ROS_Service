a288cc63badcb398a09be21434867886e5ec5c06
Divyam G <dgarg1@umd.edu>
2020-11-03 16:58:59 -0500

    Updated cpp-check and cpp-lint

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,11 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/divyam/beginner_tutorials/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/divyam/beginner_tutorials/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/divyam/anaconda3/condabin:/home/divyam/bin:/home/divyam/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PKG_CONFIG_PATH="/home/divyam/beginner_tutorials/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/divyam/beginner_tutorials/build"
+export PYTHONPATH="/home/divyam/beginner_tutorials/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/divyam/beginner_tutorials/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/divyam/beginner_tutorials/src:$ROS_PACKAGE_PATH"
