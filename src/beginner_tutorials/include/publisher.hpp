e4452cfd8d01c95a91d9144f8f7965353f89da8c
Divyam G <dgarg1@umd.edu>
2020-11-03 13:10:44 -0500

    [publisher.hpp] Edited publisher.hpp file

---
+++ src/beginner_tutorials/include/publisher.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+/**
+ * Structure to store the default display message 
+ * Type of the message is string
+ */
+struct DefaultString {
+  std::string default_msg = "Robots are awesome";
+};
