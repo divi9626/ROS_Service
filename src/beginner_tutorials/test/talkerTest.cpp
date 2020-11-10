e50805c2677984f07854933ee5e2f3a709df1cba
Divyam G <dgarg1@umd.edu>
2020-11-10 03:31:11 -0500

    [talkerTest.cpp] Added another test for service string

--- src/beginner_tutorials/test/talkerTest.cpp
+++ src/beginner_tutorials/test/talkerTest.cpp
@@ -4,23 +4,21 @@
 #include "beginner_tutorials/changeBaseString.h"
 #include "std_msgs/String.h"
 
+ros::NodeHandle nh;
+auto client = nh.serviceClient<beginner_tutorials::changeBaseString>("changeBaseString");
 /**
  * @brief test existance of service
  */
 TEST(testTalkerNode, testIfchangeBaseString) {
-    ros::NodeHandle nh;
-    auto client = nh.serviceClient<beginner_tutorials::changeBaseString>("changeBaseString");
     EXPECT_TRUE(client.waitForExistence(ros::Duration(1)));
 }
 /**
  * @brief  tests if changeBaseString service replaces default text with user input
  */
-/*TEST(testTalkerNode, testTalkerDefaultMessageUpdate) {
-    ros::NodeHandle nh;
-    auto client = nh.serviceClient<beginner_tutorials::changeBaseString>("newMessage");
-    beginner_tutorials::changeBaseString::Request request;
-    beginner_tutorials::changeBaseString::Response response;
-    request.inputString = "testString";
-    client.call(request, response);
-    EXPECT_STREQ("testString", response.modifiedString.c_str());
-}*/
+TEST(testTalkerNode, testTalkerDefaultMessageUpdate) {
+    beginner_tutorials::changeBaseString srv;
+    // change input string
+    srv.request.inputString = "testString";
+    client.call(srv.request, srv.response);
+    EXPECT_STREQ("testString", srv.response.newString.c_str());
+}
